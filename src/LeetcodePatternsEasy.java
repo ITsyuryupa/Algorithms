c693df1483cac0346d8553e56df70e2262a496ea
i.tsyuryupa <openingthedoor@yandex.ru>
2023-02-23 13:48:29 +0400

    Solved missingNumber

--- src/LeetcodePatternsEasy.java
+++ src/LeetcodePatternsEasy.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Hashtable;
 
-public class LeetcodePatterns {
+public class LeetcodePatternsEasy {
 
     public static Boolean containsDuplicateHashMap(int[] nums) {
         Hashtable<Integer, Integer> numH = new Hashtable<>();
@@ -38,5 +38,45 @@ public class LeetcodePatterns {
             }
         }
         return false;
+    }
+
+    public int missingNumberSet(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+        for (int i = 0; i <= n; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    public int missingNumberArray(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+
+        return n;
+    }
+
+    public int missingNumberSum(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+
+        for (int num : nums){
+            sum += num;
+        }
+        return (1+n)*n/2 - sum;
+    }
+
+    public int missingNumberSum2(int[] nums) {
+            return (1+nums.length)*nums.length/2 - Arrays.stream(nums).sum();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LeetcodePatterns.containsDuplicateHashSet(new int[]{1, 2, 3, 4, 5}));
+        System.out.println();
     }
 }
