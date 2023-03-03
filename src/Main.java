da58338577935b8c73ab504dc339815027275374
i.tsyuryupa <openingthedoor@yandex.ru>
2023-03-03 17:33:49 +0300

    Solved climbStairs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LeetcodePatternsEasy.singleNumberArrSort(new int[] {2,2,1}));
+        System.out.println(LeetcodePatternsEasy.climbStairsRecursion(45));
     }
 }
